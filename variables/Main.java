301d627a9ff47235083017e0259a455e88e3ab7c
esmanurbicer <115987635+esmanurbicer@users.noreply.github.com>
2022-11-08 14:01:39 +0300

    Add files via upload

---
+++ variables/Main.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int ogrenciSayisi = 12 ;
+		String metin = "Öğrenci sayısı : ";
+    System.out.println(metin + ogrenciSayisi);
+    System.out.println(metin + ogrenciSayisi);
+    System.out.println("Öğrenci sayım :" + ogrenciSayisi);
+    System.out.println("Öğrenci sayım :" + ogrenciSayisi);
+    System.out.println("Öğrenci sayım :" + ogrenciSayisi);
+	}
+
+}
