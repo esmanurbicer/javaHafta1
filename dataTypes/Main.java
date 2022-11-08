11b840c9cebba5a640f22ea96044b3fc1bb9fdfd
esmanurbicer <115987635+esmanurbicer@users.noreply.github.com>
2022-11-08 14:07:07 +0300

    Add files via upload

---
+++ dataTypes/Main.java
@@ -0,0 +1,14 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double sayi = 12.2 ;
+		char karakter = 'E' ;
+		boolean dogruMu = false ;
+		
+
+	}
+
+}
